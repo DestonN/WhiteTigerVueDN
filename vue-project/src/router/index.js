8c60f8c76f1908494c08ac23d77f53f8664187b5
Diego Escobar <diego@imtechnologyconsulting.com>
2023-11-13 12:58:47 -0600

    Index update

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -23,15 +23,6 @@ const router = createRouter({
       props: true,
       component: () => import('../views/FullDash.vue')
     },
-
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
